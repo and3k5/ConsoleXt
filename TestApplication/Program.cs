0973c44330c574744cec3afae464d9214deb5943
Anders K <and3k5@users.noreply.github.com>
2022-03-15 10:10:58 +0100

    Add test application

---
+++ TestApplication/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using And3k5.ConsoleExtensions;
+
+namespace TestApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleXt
+                .Fg(ConsoleColor.Red).Write("Hello ").ResetColor()
+                .Fg(ConsoleColor.Cyan).Write("World").ResetColor()
+                .Fg(ConsoleColor.Yellow).Write("!").ResetColor()
+                .WriteLine();
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
